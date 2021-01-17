e7d59904fc7d884530fe72932d320fb7873444a3
rumeysakdogan <dgn.rumeysaa@gmail.com>
2021-01-17 13:47:46 -0700

    QueueWithTwoStacks is added

---
+++ src/DataStructures/QueueWithTwoStacks.java
@@ -0,0 +1,39 @@
+package DataStructures;
+
+import java.util.Stack;
+
+public class QueueWithTwoStacks {
+
+    public static class MyQueue<T> {
+        private Stack<T> stackNewestOnTop = new Stack<>();
+        private Stack<T> stackOldestOnTop = new Stack<>();
+
+
+        public void enqueue(T value){ // add item
+        stackNewestOnTop.push(value);
+        }
+
+        public T peek() { // Get "oldest" item
+            // move elements from stackNewestOnTop to stackOldestOnTop
+            shiftStacks();
+            return stackOldestOnTop.peek();
+        }
+
+        private void shiftStacks(){
+            if (stackOldestOnTop.isEmpty()) {
+                while (!stackNewestOnTop.isEmpty()){
+                    stackOldestOnTop.push(stackNewestOnTop.pop());
+                }
+            }
+        }
+
+        public T dequeue() { // Get "oldest" item and remove it
+            // move elements from stackNewestOnTop to stackOldestOnTop
+            shiftStacks();
+            return stackOldestOnTop.pop();
+
+        }
+    }
+
+
+}
